187b277104e009528223975f38b11ec92bd94d60
chauchung <zc22045@gmail.com>
2025-03-31 12:50:26 +0800

    nowcoder --BC48.字母大小写转换

--- code_06_18/upper_lower_convert.c
+++ code_06_18/upper_lower_convert.c
@@ -1,5 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
+
+#include <stdio.h>
+
 int main()
 {
 	char ch;
@@ -9,7 +11,9 @@ int main()
 			ch -= 32;
 		else if (ch >= 65 && ch <= 106)
 			ch += 32;
+
 		putchar(ch);
 	}
+
 	return 0;
 }

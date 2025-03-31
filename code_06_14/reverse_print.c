83c00f293c80ba276ecc7afa55e31d4fa3e06dad
cchung-Uka <chauchung0222@gmail.com>
2025-03-31 14:26:09 +0800

    nowcoder --BC92.逆序输出

---
+++ code_06_14/reverse_print.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	int n[10] = { 0 };
+
+	for (int i = 0; i < 10; i++)
+	{
+		scanf("%d", &n[i]);
+	}
+	for (int i = 9; i >= 0; i--) 
+	{
+		printf("%d ", n[i]);
+	}
+	
+	return 0;
+}

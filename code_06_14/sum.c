601439491c6dee660f2b7293b44570f35d2eb7bd
cchung-Uka <chauchung0222@gmail.com>
2025-03-31 14:27:52 +0800

    nowcoder --BC112.小乐乐求和

---
+++ code_06_14/sum.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	long long n = 0;
+	long long sum = 0;
+	long long i = 1;
+
+	scanf("%lld", &n);
+	while (i <= n)
+	{
+		sum += i;
+		i++;
+	}
+	printf("%lld", sum);
+	
+	return 0;
+}

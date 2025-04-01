8688d4d42f06b97cacb0a9f0dba45cc058ab5c1e
cchung-Uka <chauchung0222@gmail.com>
2025-04-01 21:29:57 +0800

    9×9乘法口诀表

---
+++ code_07_16/multiply_9_9.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//在屏幕上输出 9*9 乘法口诀表
+int main()
+{
+	for (int i = 1; i <= 9; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d*%d=%d ", j, i, i * j);
+
+            if (i == j)
+                printf("\n");
+        }
+    }
+	return 0;
+}

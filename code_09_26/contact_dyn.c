f6d2a3b309ab611f9ac6b16e26ddaa85f6a0c6d4
cchung-Uka <chauchung0222@gmail.com>
2025-04-26 09:45:30 +0800

    实现通讯录（动态分配空间）

---
+++ code_09_26/contact_dyn.c
@@ -0,0 +1,274 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "contact_dyn.h"
+
+void Init_Contact(Contact* pc)
+{
+	assert(pc);
+
+	pc->sz = 0;
+	pc->capacity = DEFAULT_SZ;
+	pc->data = calloc(pc->capacity, sizeof(Peo_Info));
+
+	if (pc->data == NULL)
+	{
+		perror("Init_Contact->calloc");
+		return;
+	}
+}
+
+static void Check_capacity(Contact* pc)
+{
+	if (pc->sz == pc->capacity)
+	{
+		Peo_Info* ptr = realloc(pc->data, (pc->capacity + EXPAND) * sizeof(Peo_Info));
+		if (ptr != NULL)
+		{
+			pc->data = ptr;
+			pc->capacity += EXPAND;
+			printf("Expansion successful.\n");
+		}
+		else
+		{
+			perror("Add_Contact->realloc");
+			return;
+		}
+	}
+}
+
+void Add_Contact(Contact* pc)
+{
+	assert(pc);
+
+	Check_capacity(pc);
+
+	printf("Enter name:>");
+	scanf("%s", pc->data[pc->sz].name);
+
+	printf("Enter sex:>");
+	scanf("%s", pc->data[pc->sz].sex);
+
+	printf("Enter age:>");
+	scanf("%d", &(pc->data[pc->sz].age));
+
+	printf("Enter tel:>");
+	scanf("%s", pc->data[pc->sz].tel);
+
+	printf("Enter address:>");
+	scanf("%s", pc->data[pc->sz].addr);
+
+	pc->sz++;
+
+	printf("Addition has been achieved.\n");
+}
+
+static int Find_by_Name(Contact* pc, char* name)
+{
+	assert(pc);
+
+	for (int i = 0; i < pc->sz; i++)
+	{
+		if (strcmp(pc->data[i].name, name) == 0)
+			return i;
+	}
+	return -1;//没找到
+}
+
+void Del_Contact(Contact* pc)
+{
+	assert(pc);
+
+	char name[NAME_MAX];
+
+	if (pc->sz == 0)
+	{
+		printf("Contact is empty!\n");
+		return;
+	}
+
+	printf("Enter the name of ppl. to be deleted:>");
+	scanf("%s", name);
+
+	int ret = Find_by_Name(pc, name);
+	if (ret == -1)
+	{
+		printf("The people does not exist!\n");
+		return;
+	}
+
+	for (int i = ret; i < pc->sz - 1; i++)
+	{
+		pc->data[i] = pc->data[i + 1];
+	}
+
+	pc->sz--;
+
+	printf("Deletion has been achieved.\n");
+}
+
+void Search_Contact(Contact* pc)
+{
+	assert(pc);
+
+	char name[NAME_MAX];
+
+	if (pc->sz == 0)
+	{
+		printf("Contact is empty!\n");
+		return;
+	}
+
+	printf("Enter the name of ppl. to be searched:>");
+	scanf("%s", name);
+
+	int ret = Find_by_Name(pc, name);
+	if (ret == -1)
+	{
+		printf("The people does not exist!\n");
+		return;
+	}
+
+	printf("%-10s%-4s%-4s%-12s%-20s\n", "NAME", "SEX", "AGE", "TEL", "ADDR");
+	printf("%-10s%-4s%-4d%-12s%-20s\n",
+		pc->data[ret].name, pc->data[ret].sex, pc->data[ret].age, pc->data[ret].tel, pc->data[ret].addr);
+}
+
+void Update_menu()
+{
+	printf("--------------------------------\n");
+	printf("---- 1. Name      2. Sex    ----\n");
+	printf("---- 3. Age       4. Tel    ----\n");
+	printf("---- 5. Address   0. Exit   ----\n");
+	printf("--------------------------------\n");
+}
+
+void Update_Contact(Contact* pc)
+{
+	assert(pc);
+
+	char name[NAME_MAX];
+
+	if (pc->sz == 0)
+	{
+		printf("Contact is empty!\n");
+		return;
+	}
+
+	printf("Enter the name of ppl. to be updated:>");
+	scanf("%s", name);
+
+	int ret = Find_by_Name(pc, name);
+	if (ret == -1)
+	{
+		printf("The people does not exist!\n");
+		return;
+	}
+
+	int input = 0;
+	do
+	{
+		Update_menu();
+		printf("Enter the option you want to update:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case NAME:
+			system("cls");
+			printf("Enter name:>");
+			scanf("%s", pc->data[ret].name);
+			break;
+		case SEX:
+			system("cls");
+			printf("Enter sex:>");
+			scanf("%s", pc->data[ret].sex);
+			break;
+		case AGE:
+			system("cls");
+			printf("Enter age:>");
+			scanf("%d", &(pc->data[ret].age));
+			break;
+		case TEL:
+			system("cls");
+			printf("Enter tel:>");
+			scanf("%s", pc->data[ret].tel);
+			break;
+		case ADDR:
+			system("cls");
+			printf("Enter address:>");
+			scanf("%s", pc->data[ret].addr);
+			break;
+		case EXIT:
+			system("cls");
+			break;
+		default:
+			break;
+		}
+	} while (input);
+
+	printf("Update has been achieved.\n");
+}
+
+void Display_Contact(Contact* pc)
+{
+	assert(pc);
+
+	if (pc->sz == 0)
+	{
+		printf("Contact is empty!\n");
+		return;
+	}
+
+	printf("%-10s%-4s%-4s%-12s%-20s\n", "NAME", "SEX", "AGE", "TEL", "ADDR");
+
+	for (int i = 0; i < pc->sz; i++)
+	{
+		printf("%-10s%-4s%-4d%-12s%-20s\n",
+			pc->data[i].name, pc->data[i].sex, pc->data[i].age, pc->data[i].tel, pc->data[i].addr);
+	}
+}
+
+int Cmp_PeoInfo_Name(const void* e1, const void* e2)
+{
+	return strcmp(((Peo_Info*)e1)->name, ((Peo_Info*)e2)->name);
+}
+
+void Sort_Contact(Contact* pc)
+{
+	assert(pc);
+
+	if (pc->sz == 0)
+	{
+		printf("Contact is empty!\n");
+		return;
+	}
+
+	qsort(pc->data, pc->sz, sizeof(Peo_Info), Cmp_PeoInfo_Name);
+
+	printf("Order has been achieved.\n");
+}
+
+void Clear_Contact(Contact* pc)
+{
+	assert(pc);
+
+	if (pc->sz == 0)
+	{
+		printf("Contact is empty!\n");
+		return;
+	}
+
+	memset(pc->data, 0, sizeof(pc->data));
+	pc->sz = 0;
+
+	printf("Emptying has been achieved.\n");
+}
+
+void Destroy_Contact(Contact* pc)
+{
+	assert(pc);
+
+	free(pc->data);
+	pc->data = NULL;
+	pc->sz = 0;
+	pc->capacity = 0;
+}
---
+++ code_09_26/contact_dyn.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#define NAME_MAX 10
+#define SEX_MAX 5
+#define TEL_MAX 12
+#define ADDR_MAX 20
+
+#define DEFAULT_SZ 2;
+#define EXPAND 2
+
+typedef struct Peo_Info
+{
+	char name[NAME_MAX];
+	char sex[SEX_MAX];
+	int age;
+	char tel[TEL_MAX];
+	char addr[ADDR_MAX];
+}Peo_Info;
+
+//动态
+typedef struct Contact
+{
+	Peo_Info* data;//接收calloc开辟的内存块初始位置
+	int sz;//记录当前通讯录联系人信息的个数
+	int capacity;//记录当前通讯录的容量
+}Contact;
+
+enum Option
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	UPDATE,
+	DISPLAY,
+	SORT,
+	CLEAR,
+};
+
+enum Option_update
+{
+	NAME = 1,
+	SEX,
+	AGE,
+	TEL,
+	ADDR,
+};
+
+//初始化通讯录
+void Init_Contact(Contact* pc);
+
+//添加联系人信息
+void Add_Contact(Contact* pc);
+
+//删除指定联系人
+void Del_Contact(Contact* pc);
+
+//查找指定联系人
+void Search_Contact(Contact* pc);
+
+//更新指定联系人信息
+void Update_Contact(Contact* pc);
+
+//显示所有联系人
+void Display_Contact(Contact* pc);
+
+//联系人信息以名字顺序进行排序
+void Sort_Contact(Contact* p);
+
+//清空联系人信息
+void Clear_Contact(Contact* pc);
+
+//销毁通讯录
+void Destroy_Contact(Contact* pc);
